35e8dc3b34f6928ca27e941bcba2fcb050c5a0ef
Juliany Savazzi <julianysaz@gmail.com>
2020-05-04 20:40:47 -0300

    Trying use firebase function

--- functions/index.js
+++ functions/index.js
@@ -2,43 +2,56 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().functions);
+//admin.initializeApp(functions.config());
 
-var newData;
+//var newData;
+var deviceTokens;
 
-exports.messageTrigger = functions.firestore.document('call/{receiverId}').onCreate( async (snapshot, context) => {
+exports.sendToDevice = functions.firestore.document('call/{receiverId}').onCreate( async (snapshot, context) => {
+//exports.messageTrigger = functions.firestore.document('call/{receiverId}').onWrite( async (snapshot, context) => {
     if(snapshot.empty){
         console.log('No Devices found!');
         return;
     }
 
-    var tokens = [];
+    //var tokens = [];
 
-    newData = snapshot.data;
+    //newData = snapshot.data;
 
-    const deviceTokens = await
-        admin.firestore().collection('call').document('call/{receiverId}')
-        .collection('users').document('users/{token}')
-        .get();
+    //var token = await admin.firestore().document('users/{token}').get();
 
-    for (var token of deviceTokens) {
-        tokens.push(token.data().device_token);
-    }
+    //const deviceTokens = await
+    //    admin.firestore().collection('call').document('call/{receiverId}')
+    //    .collection('users').document('users/{token}')
+    //    .get();
+
+    deviceTokens = await
+            admin.firestore().collection('call').document('call/{receiverId}')
+            .collection('users').document('users/{token}')
+            .get();
+
+    //for (var token of deviceTokens) {
+    //    tokens.push(token.data().device_token);
+    //}
 
     var payload = {
         notification: {
             title: 'Ligação do Luzia', body: 'Alguém precisa da sua ajuda!', sound: 'default'
         },
         data: {
-            click_action: 'FLUTTER_NOTIFICATION_CLICK', message: newData.message
+            click_action: 'FLUTTER_NOTIFICATION_CLICK', //message: newData.message
         },
     };
     try{
-        const response = await admin.messaging().sendToDevice(tokens, payload);
+        //const response = await admin.messaging().sendToDevice(token, payload);
+        const response = await admin.messaging().sendToDevice(deviceTokens, payload);
+        //const response = await admin.messaging().sendToDevice(tokens, payload);
         console.log('Notification sent succesfully');
     } catch(err){
         console.log('Error sending notification');
     }
 }) ;
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions

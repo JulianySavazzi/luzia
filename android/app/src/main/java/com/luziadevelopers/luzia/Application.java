b9b2fe949b7c21e07cb9ce921026c4379a3f8ef9
Mariugo Megiani <mariugomegiani@gmail.com>
2020-05-05 19:55:09 -0300

    Removing agora.io settings from application.java

--- android/app/src/main/java/com/luziadevelopers/luzia/Application.java
+++ android/app/src/main/java/com/luziadevelopers/luzia/Application.java
@@ -1,20 +1,12 @@
 package com.luziadevelopers.luzia;
 
 import io.flutter.app.FlutterApplication;
-import androidx.annotation.NonNull;
-import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
 
 public class Application extends FlutterApplication implements PluginRegistrantCallback {
-
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine); //alteração do agora.sdk
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
